b0d13f2a36ddcc10aba8e535c8472a7b6fd9761a
Саша Саша <Sashapronehoda@gmail.com>
2021-11-11 21:36:09 +0200

    Add project files.

---
+++ WebApi/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Models
+{
+    public class Employee
+    {
+
+        public int EmplyeeID { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string DateOfJoining { get; set; }
+
+        public string Department { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
